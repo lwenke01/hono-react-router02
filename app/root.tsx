95a0360ae549eb89c4164f5456509c7b16280531
Lisa Wenke <lisa.wenke@slalom.com>
2026-05-19 18:26:40 +0100

    added favicon

--- app/root.tsx
+++ app/root.tsx
@@ -58,7 +58,8 @@ export default function App() {
 
   return (
     <>
-<nav className="navbar navbar-expand-lg  bg-danger-subtle border-bottom border-body">
+<nav className="navbar navbar-expand-lg fixed-top bg-danger-subtle border-bottom border-body">
+  <img src="..." className="object-fit-xxl-contain border rounded" alt="..."></img>
   <div className="container-fluid">
     <a className="navbar-brand" href="#">Navbar</a>
      <a className="navbar-brand" href="#">Navbar</a>
@@ -96,37 +97,8 @@ export default function App() {
   </div>
 </nav>
       <header className="container d-flex px-4 py-8">
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar1</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+        xx
+   
         {/* {isPending ? null : session ? (
           <div className="flex items-center gap-4">
             <span className="text-sm">{session.user.email}</span>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -193,44 +193,7 @@ export default function Home({ loaderData }: Route.ComponentProps) {
   return (
 
     <main className="container flex min-h-screen items-start justify-center p-8">
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li className="dropdown-divider"></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-      <div className="tab-content" id="pills-tabContent">
-  <div className="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+   
 
  
       <div className="w-full max-w-6xl grid grid-cols-1 lg:grid-cols-4">
@@ -583,11 +546,7 @@ export default function Home({ loaderData }: Route.ComponentProps) {
           </button>
         </div>
       )}
-      </div>
-        <div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">"Hello"</div>
-  <div className="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab" >...</div>
-  <div className="tab-pane fade" id="pills-disabled" role="tabpanel" aria-labelledby="pills-disabled-tab" >...</div>
-</div>
+     
     </main>
   )
 }
